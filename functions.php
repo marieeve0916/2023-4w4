b2ec5fe98238c0b53cfd37a35457bd8019b3dd2e
Marie-Ève Pelletier <e2142821@cmaisonneuve.qc.ca>
2023-01-26 11:33:12 -0500

    Ajout de add_action

--- functions.php
+++ functions.php
@@ -1,8 +1,11 @@
 <?php
 // L'ensemble des fonctions du thème
+function enfiler_css() {
 wp_enqueue_style('main-styles', //identificateur
                 get_template_directory_uri() . '/style.css', //adresse uri de style.css
                 array(), // définir les dépendances
                 fileatime(get_template_directory() . '/style.css'), //le calcul de la version du fichier css
-                'all') //media
+                'all'); //media
+}
+add_action('wp_enqueue_scripts', 'enfiler_css');
 ?>
--- index.php
+++ index.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>4w4</title>
-    <?php wp_head() ?>
-</head>
-<body>
-    <h1>Bienvenue sur 4w4</h1>
+<?php
+// index.php est le modèle par défaut du thème 4w4
+    get_header() 
+?>
+<main>
+        <h1>Bienvenue sur 4w4</h1>
+    </main>
+<?php get_footer() ?>
 </body>
 </html>
