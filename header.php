ba85a680f0c5271406dd134477098349b11b1472
marieeve0916 <e2142821@cmaisonneuve.qc.ca>
2023-03-12 20:51:49 -0400

    TP1 Modification style main

--- header.php
+++ header.php
@@ -5,19 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>4w4</title>
-    <!-- <a class="site__titre" href="<?= bloginfo('url'); ?>">
-    <h1 class="site__info"><?= bloginfo('name'); ?></h1></a>
-    <h2 class="site__desc"><?= bloginfo('description'); ?></h2> -->
+    <!-- Liens des icônes -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <?php wp_head() ?>
 </head>
 <body>
 <body class="site <?php echo(is_front_page() ? "no__aside" : "");?>" >
     <header class="site__entete">
         <div class="site__entete__haut">
-            <!-- <div class="site__entete__textes">
-                <h1><a class="site__titre" href="<?= bloginfo("url"); ?>"><?= bloginfo("name"); ?></a></h1>
-                <h2 class="site__desc"><?= bloginfo("description"); ?></h2>
-            </div> -->
             <div class="logo__site">
                 <?php the_custom_logo(); ?> 
             </div>
@@ -29,8 +24,17 @@
                         )); 
                 ?>
             </div>
-            <div class="barre__recherche">
-                <?php get_search_form(); ?>
+            <div class="header__cote__droit">
+                <div class="barre__recherche">
+                    <?php get_search_form(); ?>
+                </div>
+                <div>
+                    <input type="checkbox" id="cc-pour-bouton-burger">
+                    <label for="cc-pour-bouton-burger" class="bouton-burger">
+                        <span class="material-symbols-outlined icone-btn-burger-ouvrir icone" id="menu">menu</span>
+                        <span class="material-symbols-outlined icone-btn-burger-fermer icone" id="fermer-menu"> close </span>
+                    </label>
+                </div>
             </div>
         </div>
     </header>
