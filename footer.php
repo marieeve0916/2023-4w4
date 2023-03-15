165aa8b076e8abb88884d550870ad7cb15c0c41d
marieeve0916 <e2142821@cmaisonneuve.qc.ca>
2023-03-15 18:00:59 -0400

    TP1 Modification main mobile

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer  class="site__footer">
     <section class="lien">1</section>
     <section class="lien">2</section>
-    <section class="lien">3</section>
+    <section class="logo__footer"><?php the_custom_logo(); ?></section>
 </footer>
     <?php wp_footer() ?>
