412bb7a79a069d170830ba0a53f28f6dad77e72d
Marie-Ève Pelletier <e2142821@cmaisonneuve.qc.ca>
2023-03-30 10:29:36 -0400

    Étape 2 - Premiers changements scss

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 // index.php est le modèle par défaut du thème 4w4
     get_header() 
 ?>
-<main  class="site__main">
+<main class="site__main custom-background"> 
     <pre>404.php</pre>
     <h1>Erreur 404</h1>
     <h4>Page introuvable. Vous pouvez tenter une recherche:</h4>
@@ -10,13 +10,15 @@
         <?php get_search_form(); ?>
     </div>
     <h5>Nos choix de cours</h5>
-    <?php
-        wp_nav_menu(array(
-            'menu'				=> "cours",
-            'container'			=> "nav", 
-            'container_class'	=> "menu_cours"
-        ) );
-    ?>
+    <div class="menu__cours">
+        <?php
+            wp_nav_menu(array(
+                'menu'				=> "cours",
+                'container'			=> "nav", 
+                'container_class'	=> "menu_cours"
+            ) );
+        ?>
+    </div>
     <h5>Les notes de cours</h5>
     <?php
         wp_nav_menu(array(
@@ -25,7 +27,6 @@
             'container_class'	=> "menu_4w4"
         ) );
     ?>
-
 </main>
 <?php get_footer() ?>
 </body>
