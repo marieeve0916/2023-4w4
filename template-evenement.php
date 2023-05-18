01815faa7b1bfa57233ec3e3060b3fb110114e0e
marieeve0916 <e2142821@cmaisonneuve.qc.ca>
2023-05-18 11:31:45 -0400

    Changements styles footer et menus

--- template-evenement.php
+++ template-evenement.php
@@ -4,15 +4,15 @@
      */
 
     get_header(); ?>
-    <main class="site__main">
+    <main class="site__main site__evenement">
     <?php
-    if ( have_posts() ) : the_post(); ?>
-    <?php the_post_thumbnail('medium'); ?>
-    <h1><?= get_the_title(); ?></h1>
-    <?php the_content();?>
-    <p>L'adresse de l'evénement<?php the_field('adresse'); ?></p>
-    <p>La date et l'heure de l'événement<?php the_field('dateetheure'); ?></p>     
-    <?php endif;?>
+        if ( have_posts() ) : the_post(); ?>
+        <figure class="img__evenement"><?php the_post_thumbnail('medium'); ?></figure>
+        <h1 class="titre__evenement"><?= get_the_title(); ?></h1>
+        <?php the_content();?>
+        <p class="adresse__evenement">L'adresse de l'evénement: <?php the_field('adresse'); ?></p>
+        <p class="date__evenement">La date et l'heure de l'événement: <?php the_field('dateetheure'); ?></p>     
+        <?php endif;?>
     </main><!-- #main -->
     <?php
     get_footer();
